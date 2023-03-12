d65b723c5e343462864743d5b4400d02c7533543
Eli Saado <eli5saado@gmail.com>
2023-03-12 20:09:34 +0100

    fix bugs and make app prod ready

--- alles_dlp/main.py
+++ alles_dlp/main.py
@@ -8,12 +8,18 @@ import yt_dlp
 from validators import url as url_validator
 import os
 
-passwords = os.environ.get('ALLES_DLP_PASSWORDS').split(',')
+try:
+    passwords = os.environ.get('ALLES_DLP_PASSWORDS').split(',')
+except:
+    raise Exception('ALLES_DLP_PASSWORDS not set')
 
 opts = {
     "quiet":    True,
     "simulate": True,
     "forceurl": True,
+    # don't download playlists, as it doesn't really work with the ios shortcut
+    'noplaylist': True,
+    'playlist_items': '1'
 }
 
 if os.path.exists('cookies/cookies.txt'):
@@ -28,26 +34,45 @@ async def homepage(request: Request):
 
     url = request.query_params.get('url')
     if url is None:
-        return JSONResponse({'error': 'no'})
+        return JSONResponse({'error': 'no url'})
 
     if not url_validator(url):
         return JSONResponse({'error': 'invalid url'})
 
     with yt_dlp.YoutubeDL(opts) as ydl:
-        info = ydl.sanitize_info(ydl.extract_info(url))
+        try:
+            info = ydl.sanitize_info(ydl.extract_info(url, download=False))
+            print(info)
+            formats = info["formats"]
 
-        formats = info["formats"]
+            viable = []
+            if (len(formats) == 1):
+                viable = formats
+            else:
+                for format in formats:
+                    vcodec_none = (
+                        'vcodec' in format and format['vcodec'] == 'none')
+                    video_ext_none = (
+                        'video_ext' in format and format['video_ext'] == 'none')
 
-        viable = []
-        if (len(formats) == 1):
-            viable = formats
-        else:
-            viable = list(filter(
-                lambda format: (('vcodec' in format and format['vcodec'] != 'none') or ('video_ext' in format and format['video_ext'] != 'none')) and (format['acodec'] != 'none' if 'acodec' in format else True) and ("m3u" not in format['protocol'] if 'protocol' in format else True), formats))
+                    if vcodec_none and video_ext_none:
+                        format['video_ext'] += " (no video)"
 
-        return JSONResponse({"data": viable})
+                    # skip anything without audio
+                    if 'acodec' in format and format['acodec'] == 'none':
+                        continue
 
-app = Starlette(debug=True, routes=[
+                    # skip m3u8
+                    if 'protocol' in format and "m3u" in format['protocol']:
+                        continue
+
+                    viable.append(format)
+
+            return JSONResponse({"data": viable})
+        except Exception as e:
+            return JSONResponse({'error': repr(e)})
+
+app = Starlette(debug=not not os.environ.get('ALLES_DLP_DEBUG', False), routes=[
     Route('/', homepage),
 ])
 
